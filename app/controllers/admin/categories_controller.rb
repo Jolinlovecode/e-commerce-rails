f72c513126b0d5b766c4e752ffe6c0d48d3122f4
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-11 15:07:36 -0700

    add admin-categories controller and route

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,23 @@
+class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV["username"], :password => ENV["password"]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+end
