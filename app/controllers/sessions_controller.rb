98c5aba3ddcd20f6b7c45248461007075ee519ce
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-03-12 11:52:45 -0700

    add session create and destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+  end 
+
+  def create
+    # use user email to find user firstly
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      # if user exists and the input password are correct,save user id inside the brower cookie. 
+      # permit them login when they navigate into this website.
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      # otherwise send the login form to the user
+      redirect_to "login"
+    end
+  end 
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
+  end
+  
 end
